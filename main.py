b9ca1fcf9dffeadf58941ba2012fa3ae8e4098de
fazaminan013-eng <fazaminan013@gmail.com>
2026-05-11 01:05:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from telegram_notifier import TelegramNotifier
 
 def main():
     TOKEN = os.environ.get("TOKEN")
-    CHAT_ID = os.environ.get(6468207840)
+    CHAT_ID = os.environ.get("CHAT_ID")
 
     if not TOKEN or not CHAT_ID:
         print("Missing TOKEN or CHAT_ID")
